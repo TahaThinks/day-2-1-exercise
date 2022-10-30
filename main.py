77132fcd03ec6e408c6d1d26cbba43298f0d3fca
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-10-30 19:44:40 +0000

    Increased the number of lines, but made the code much clearer.

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ two_digit_number = input("Type a two digit number: ")
 ####################################
 #Write your code below this line 👇
 
-print(int(two_digit_number[0])+int(two_digit_number[1]))
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+result = int(first_digit) + int(second_digit)
+print(result)
